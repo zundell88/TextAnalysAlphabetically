cdb1a5ffc1881b33e65c42589313a1155097446c
Christofer Sundell <coffasund@gmail.com>
2016-10-25 09:30:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysAlphabetically
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader boyeStreamReader = null;
+            SortedSet<string> ordlista = new SortedSet<string>(File.ReadAllLines("ordlista.txt", Encoding.Default));
+            
+            try
+            {
+                boyeStreamReader = new StreamReader("Boye.txt", Encoding.Default);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Couldn't find the file.");
+                Console.ReadLine();
+                return;
+            }
+
+            var frequencyDictionary = new SortedDictionary<string, int>(StringComparer.CurrentCultureIgnoreCase);
+            string lineOfWords;
+            while ((lineOfWords = boyeStreamReader.ReadLine()) != null)
+            {
+                string[] wordArray = ExtractWords(lineOfWords);
+                foreach (var word in wordArray)
+                {
+                    if (word != "")
+                    {
+                        int n = 0;
+                        frequencyDictionary.TryGetValue(word, out n);
+                        frequencyDictionary[word] = n + 1;
+                    }
+                }
+            }
+            boyeStreamReader.Close();
+
+            foreach (KeyValuePair<string, int> pair in frequencyDictionary)
+            {
+                bool match = ordlista.Contains(pair.Key);
+
+                if (match == false)
+                    Console.WriteLine($"{pair.Key}");
+            }
+        }
+        public static string[] ExtractWords(string inputString)
+        {
+            char[] charArray = inputString.ToCharArray();
+            charArray = Array.FindAll<char>(charArray, (c => (char.IsLetter(c) || c == ' ')));
+            string wordString = new string(charArray);
+            return wordString.Split(' ');
+        }
+    }
+}
